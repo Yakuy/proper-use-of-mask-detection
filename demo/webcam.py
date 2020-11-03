c9ad5b5caf2d179bd65f9d2072ee7eda6fa2844d
Chrysophyt <ian240201@gmail.com>
2020-11-03 14:38:37 +0700

    Init repo

---
+++ demo/webcam.py
@@ -0,0 +1,44 @@
+import cv2
+import time
+from imutils.video import FPS, WebcamVideoStream
+
+
+def startWebCam():
+    stream = WebcamVideoStream(src=0).start()  # default camera
+    time.sleep(1.0)
+    # start fps timer
+    # loop over frames from the video file stream
+    while True:
+        # grab next frame
+        frame = stream.read()
+        key = cv2.waitKey(1) & 0xFF
+
+        # update FPS counter
+        fps.update()
+        #frame = predict(frame)
+
+        # keybindings for display
+        if key == ord('p'):  # pause
+            while True:
+                key2 = cv2.waitKey(1) or 0xff
+                cv2.imshow('frame', frame)
+                if key2 == ord('p'):  # resume
+                    break
+        cv2.imshow('frame', frame)
+        if key == 27:  # exit
+            break
+
+
+if __name__ == '__main__':
+
+    fps = FPS().start()
+    startWebCam()
+    # stop the timer and display FPS information
+    fps.stop()
+
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+    # cleanup
+    cv2.destroyAllWindows()
+    
